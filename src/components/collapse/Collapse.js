cd1f2f161ae72222fe2620c78f1c0f19cbd0c966
mosaber30 <mhd.hawk@gmail.com>
2024-10-27 20:51:40 +0100

    collapse animation

--- src/components/collapse/Collapse.js
+++ src/components/collapse/Collapse.js
@@ -2,36 +2,25 @@ import React, { useState } from "react";
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
 const Collapse = ({ title, content }) => {
-  // Declaration of the state using the useState() Hook
-  const [isOpen, setIsOpen] = useState(true); // I define the state for the toggle (set to false by default)
+  const [isOpen, setIsOpen] = useState(false); // Keep the initial state as true
 
-  // Function to handle the display of the collapse content
-  const display = () => {
+  const toggleCollapse = () => {
     setIsOpen(!isOpen);
   };
 
   return (
-    // Displays the collapsed section by default and opens it on click, then closes it again on click, hiding the text and style
-    <div className="collapse__dropdown__container"> 
-     {/* to add a class when isopen is true  */}
-      <div className="collapse__dropdown__title">
+    <div className={`collapse__dropdown__container ${isOpen ? "open" : ""}`}>
+      <div className="collapse__dropdown__title" onClick={toggleCollapse}>
         <h2>{title}</h2>
-        <p onClick={display}>
-          {isOpen ? (
-            <i className="fa-solid fa-chevron-down"></i>
-          ) : (
-            <i className="fa-solid fa-chevron-up"></i>
-          )}
+        <p>
+          <i className={`fa-solid ${isOpen ? "fa-chevron-up" : "fa-chevron-down"}`}></i>
         </p>
       </div>
-      {/* If the collapse state is TRUE, it will display the description */}
-      <div className="collapse__dropdown__content">
-     
+      <div className={`collapse__dropdown__content ${isOpen ? "show" : "hide"}`}>
         <p>{content}</p>
       </div>
     </div>
   );
 };
-
 
 export default Collapse;
